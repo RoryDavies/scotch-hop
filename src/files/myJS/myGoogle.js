a45bf3f46b5bb06a1dea340741dbf4718324d235
Rory Davies <rory.davies@rorysco.co.uk>
2016-01-24 15:22:17 +0000

    Added DocPad files and folders. Moved some files into docpad src dir to make it work. Target for web is now the "out" dir (index.html). Updated .gitignore to ignore the docpad modules (for now at least) and the Bootstrap code (for now at least).

---
+++ src/files/myJS/myGoogle.js
@@ -0,0 +1,72 @@
+jQuery(function($) {
+    // Asynchronously Load the map API 
+    var script = document.createElement('script');
+    script.src = "http://maps.googleapis.com/maps/api/js?sensor=false&callback=initialize";
+    document.body.appendChild(script);
+});
+
+function initialize() {
+    var map;
+    var bounds = new google.maps.LatLngBounds();
+    var mapOptions = {
+        mapTypeId: 'roadmap',
+        center: {lat: 55.9785, lng: -3.6006585},
+        zoom: 8
+// not sure if zoom has any effect with markers set.
+    };
+                    
+    // Display a map on the page
+    map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+    map.setTilt(45);
+        
+    // Multiple Markers
+    var markers = [
+//        ['Home', 55.9644488,-3.440138],
+        ['Linlithgow Palace', 55.9785,-3.6006585],
+        ['Low Port Centre', 55.9775,-3.5967]
+    ];
+                        
+    // Info Window Content
+    var infoWindowContent = [
+        ['<div class="info_content">' +
+        '<h3>Linithgow Palace EH49 7AL</h3>' +
+        '<p>The Palace has some parking just outside. Further parking is available nearby.</p>' +
+        '</div>'],
+        ['<div class="info_content">' +
+        '<h3>Low Port Centre EH49 7HZ</h3>' +
+        '<p>The Low Port Centre, opposite Tesco, has limited parking.</p>' +
+        '</div>']
+    ];
+        
+    // Display multiple markers on a map
+    var infoWindow = new google.maps.InfoWindow(), marker, i;
+    
+    // Loop through our array of markers & place each one on the map  
+    for( i = 0; i < markers.length; i++ ) {
+        var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
+        bounds.extend(position);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: position,
+            title: markers[i][0]
+        });
+        
+        // Allow each marker to have an info window    
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infoWindow.setContent(infoWindowContent[i][0]);
+                infoWindow.open(map, marker);
+            }
+        })(marker, i));
+
+        // Automatically center the map fitting all markers on the screen
+        map.fitBounds(bounds);
+    }
+
+    // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
+    var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
+        this.setZoom(14);
+        google.maps.event.removeListener(boundsListener);
+    });
+    
+}
